b2bb7de832ecab66637f4ec5bcc9d20e27a7ee92
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-10-22 06:33:39 +0300

    Copied classes from client to the server
    -gameobject
    -player
    -puck
    -vector

---
+++ server/src/Player.ts
@@ -0,0 +1,63 @@
+import { GameObject } from "./GameObject";
+import { Vector } from "./Vector";
+
+export class Player extends GameObject {
+  //x and y from previous frame. Used for calculating velocity
+  public xPrev: number = 0;
+  public yPrev: number = 0;
+
+  handleMouseMove(
+    event: MouseEvent,
+    canvas: HTMLCanvasElement,
+    isMouseDown: boolean,
+  ) {
+    if (!isMouseDown) {
+      this.xPrev = this.x;
+      this.yPrev = this.y;
+      return;
+    }
+
+    const rect = canvas.getBoundingClientRect(); // Get canvas bounds
+    const mouseX = event.clientX - rect.left; // Mouse X relative to the canvas
+    const mouseY = event.clientY - rect.top; // Mouse Y relative to the canvas
+
+    // Calculate velocity based on the difference between the new mouse position and the player's current position
+    this.xPrev = this.x;
+    this.yPrev = this.y;
+
+    // Limit player position to inside the canvas and below the center line
+    this.x = Math.min(
+      Math.max(this.radius, mouseX),
+      canvas.width - this.radius,
+    );
+    this.y = Math.min(
+      Math.max(canvas.height / 2 + this.radius, mouseY),
+      canvas.height - this.radius,
+    );
+  }
+
+  handleTouchMove(
+    event: TouchEvent,
+    canvas: HTMLCanvasElement,
+    isMouseDown: boolean,
+  ) {
+    event.preventDefault(); // Prevents scrolling the page
+
+    if (!isMouseDown) {
+      this.xPrev = this.x;
+      this.yPrev = this.y;
+      return;
+    }
+
+    canvas.dispatchEvent(
+      new MouseEvent("mousemove", {
+        clientX: event.touches[0].clientX,
+        clientY: event.touches[0].clientY,
+      }),
+    );
+  }
+
+  velocity(){
+    return new Vector(this.x - this.xPrev, this.y - this.yPrev);
+  }
+}
