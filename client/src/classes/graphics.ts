f79048ce13c311b93545a6492b2d87ff3028c6d2
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-11-05 12:24:04 +0200

    ported the newest main version to React

---
+++ client/src/classes/graphics.ts
@@ -0,0 +1,33 @@
+export const drawCenterLine = (
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+) => {
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height / 2);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.stroke();
+};
+
+export const drawCenterCircle = (
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+) => {
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2);
+    ctx.stroke();
+};
+
+export const drawGoals = (
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+) => {
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, 0, 50, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height, 50, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.closePath();
+};
--- client_old/src/types/GameState.ts
+++ client_old/src/types/GameState.ts
@@ -1,5 +1,5 @@
-import { Player } from "../classes/Player";
-import { Puck } from "../classes//Puck";
+import { Player } from "../Player";
+import { Puck } from "../Puck";
 
 export interface GameState {
   players: Player[];
